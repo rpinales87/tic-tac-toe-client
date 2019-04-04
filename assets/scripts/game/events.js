777f751faf47b201ed1df000cd03cb2aa5c3781f
rpinales87 <pinales.roberto@gmail.com>
2019-04-04 12:11:37 -0400

    Add game functionality
    - Check Board for Winner

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -31,7 +31,7 @@ const winner = function () {
       (gameBoard[0] === 'x' && gameBoard[3] === 'x' && gameBoard[6] === 'x') ||
       (gameBoard[1] === 'x' && gameBoard[4] === 'x' && gameBoard[7] === 'x') ||
       (gameBoard[2] === 'x' && gameBoard[5] === 'x' && gameBoard[8] === 'x')) {
-    console.log('x wins!')
+    console.log('X wins!')
   } else if (
     (gameBoard[0] === 'o' && gameBoard[1] === 'o' && gameBoard[2] === 'o') ||
     (gameBoard[3] === 'o' && gameBoard[4] === 'o' && gameBoard[5] === 'o') ||
@@ -41,16 +41,21 @@ const winner = function () {
     (gameBoard[0] === 'o' && gameBoard[3] === 'o' && gameBoard[6] === 'o') ||
     (gameBoard[1] === 'o' && gameBoard[4] === 'o' && gameBoard[7] === 'o') ||
     (gameBoard[2] === 'o' && gameBoard[5] === 'o' && gameBoard[8] === 'o')) {
-    console.log('o wins!')
-  } else {
+    console.log('O wins!')
+  } else if (
+    gameBoard[0] !== '' && gameBoard[1] !== '' && gameBoard[2] !== '' &&
+    gameBoard[3] !== '' && gameBoard[4] !== '' && gameBoard[5] !== '' &&
+    gameBoard[6] !== '' && gameBoard[7] !== '' && gameBoard[8] !== '') {
     console.log('It/s a tie!')
   }
 }
 
 const addHandlers = function () {
   $('.box').on('click', onPlay)
+  $('.box').on('click', winner)
 }
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  winner
 }
